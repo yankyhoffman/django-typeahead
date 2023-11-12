253b4745acd5579726171c7a4d4b7b3302db89c2
Yanky Hoffman <developer.yankyhoffman@gmail.com>
2023-11-12 12:32:26 -0500

    More random data

--- core/management/commands/load.py
+++ core/management/commands/load.py
@@ -1,24 +1,19 @@
+import itertools
+
 from django.core.management.base import BaseCommand
 
 from core.models import Person
 
-SAMPLE_DATA = [
-    ('Zaylee', 'Williamson'),
-    ('Emerson', 'Beltran'),
-    ('Kaydence', 'Hutchinson'),
-    ('Korbin', 'O’Neal'),
-    ('Treasure', 'Dyer'),
-    ('Atreus', 'Webb'),
-    ('Ariella', 'Norman'),
-    ('Aziel', 'Carey'),
-    ('Alora', 'Benton'),
-    ('Jamal', 'Frye'),
-]
+FIRST_NAMES = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Henry', 'Ivy', 'Jack', 'Katie', 'Lily',
+               'Mason', 'Nathan', 'Owen', 'Penny', 'Quinn', 'Riley', 'Sam', 'Tori', 'Ursula', 'Violet', 'Walter',
+               'Xavier', 'Yvonne', 'Zachary']
+LAST_NAMES = ['Adams', 'Brown', 'Clark', 'Davis', 'Evans', 'Flores', 'Garcia', 'Harris', 'Jackson', 'King', 'Lee',
+              'Morgan', 'Nelson', 'Parker', 'Roberts', 'Scott', 'Smith', 'Taylor', 'Thompson', 'White', 'Wright']
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Person.objects.all().delete()
 
-        for (first_name, last_name) in SAMPLE_DATA:
-            Person.objects.create(first_name=first_name, last_name=last_name)
+        for first, last in itertools.product(FIRST_NAMES, LAST_NAMES):
+            Person.objects.create(first_name=first, last_name=last)
